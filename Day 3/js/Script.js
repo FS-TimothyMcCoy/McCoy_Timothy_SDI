e76721fe9feb09ad97ec632829b623b1e04a10ec
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-10 20:18:30 -0500

    This was supposed to happen a very long time ago.
    Setup basic files and started coding.

---
+++ Day 3/js/Script.js
@@ -0,0 +1,74 @@
+/*
+Timothy McCoy 1/10/2015 Day 3 Coding
+ */
+// Single lined comment
+//This is the alert
+//alert("Testing to see if this is connected");
+//Console.log Programmers can see this
+console.log("This is the console.log");
+//Declare a variable
+//We use the keyword of var
+var whatever1;  // Declaration of a variable
+whatever1=42;  // Definition of the variable
+console.log(whatever1);
+//Declare and define a variable
+var a=2;
+console.log(a);
+//Simple Math
+a=a+3
+console.log(a);
+var b;
+b=a+3;
+console.log(b);
+console.log(a);
+// Find your age
+var yearBorn= 1996;
+//age is calculated by current year minus year born
+var currentYear=2015;
+var age=currentYear - yearBorn - 1;
+console.log(age);
+//+,-*,/
+//Find area of triangle
+//1/2 * base * height
+var base=8;
+var height=10;
+
+var areaTriangle = 1/2 * base * height;
+console.log(areaTriangle);
+//Modulo - %
+//Gives the remainder
+var remainder=32%10
+console.log(remainder);
+//Find if even or odd
+//To find if even or odd we Modulo by 2
+//1 if it is odd 0 if it is even
+var evenOrOdd=67%2;
+console.log(evenOrOdd);
+// Assignment Operators
+/*
+= Assignment
+++ Adds 1 to the variable
+-- subtracts one from the variable
++= #number Addition Assignment
+-= #number Subtraction Assignment
+/= #number Division Assignment
+*= #number Multiplication Assignment
+ */
+var counter=1;
+counter++; //counter=counter+1;
+console.log(counter);
+counter--;// counter = counter - 1;
+console.log(counter);
+counter+=3;// counter= counter + 3
+console.log(counter);
+counter-=2; //counter = counter - 2;
+console.log(counter);
+counter/=2; // counter = counter/2;     /= Divided By
+console.log(counter);
+counter*=4;// counter = counter*4;      *= Multiplied by
+console.log(counter);
+
+
+
+
+
