61ebc7d49986f89f2fc13867b60e0301ffe1c1ea
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-11 00:07:06 -0500

    Day 3 lab expression
    Day 3 lab expression in progress.

---
+++ Day3LabExpressions/js/Script2.js
@@ -0,0 +1,30 @@
+//Timothy McCoy 1/10/15 Section 4 SDI
+//Variables
+var people=8;
+var slices=6;
+var pizzas=3;
+//Total = pizzas multiplied by the amount of  slices
+var total= pizzas * slices;
+console.log(total);
+//quotient = total divided by the amount of people
+var quotient = total/ people;
+console.log("Each Person ate " + quotient + " slices of Pizza at the party.");
+
+//Creating Sparky variable
+
+var dogName= "Sparky";
+console.log(dogName);
+var remai
+
+
+
+
+
+
+
+
+
+
+
+
+
