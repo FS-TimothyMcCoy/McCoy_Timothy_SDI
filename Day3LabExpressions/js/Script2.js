7ec1b0fabd7e0f3370390d71e457042347d96455
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-11 00:10:50 -0500

    Day 3 lab expressions
    Finally completed this assignment

--- Day3LabExpressions/js/Script2.js
+++ Day3LabExpressions/js/Script2.js
@@ -25,7 +25,28 @@ var week2=35;
 var week3=50;
 var week4=13;
 var week5=48;
-//Add 
+//Add them up and divide them by the number of weeks there are
+var average= (week1+week2+week3+week4+week5) /5;
+console.log(average);
+var total2=week1+week2+week3+week4+week5;
+//Create console log statement for the whole thing
+console.log("You have spent a total of $"+total2+" on groceries over 5 weeks. That is an average of $"+ average +" per week.");
+
+
+
+//Variables
+var percentDiscountRawNumber=30;
+var tenPercentage= .1;
+var originalPrice=100;
+var thirtyPercentage= .3;
+var item="Football";
+var salesTaxPercentage= originalPrice * thirtyPercentage;
+var discountPercentage=originalPrice * tenPercentage;
+//Make Sure all values and variables are listed and declared
+// Start of Console log statement aka Result Fromat
+console.log("Your "+item+" was originally $"+originalPrice+", but after a "+percentDiscountRawNumber+"% discount, it is now $"+discountPercentage+" without tax, and $"+salesTaxPercentage+" with tax");
+
+
 
 
 
