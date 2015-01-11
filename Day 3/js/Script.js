63182d95a2ed94eda749ee9a5ae1d7e83765396e
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-10 20:39:20 -0500

    Double Quotes or Single Quotes
    We practiced double and single quoting things and their meanings.

--- Day 3/js/Script.js
+++ Day 3/js/Script.js
@@ -70,5 +70,28 @@ console.log(counter);
 
 
 
+// String - Is any text variable
+//" Is used at the beginning and end of the string"
+//Used to distinguish between variable name and random text
+//Single Quotes ' and double quotes " are both used, BUT must match "", '', etc.
+
+
+
+var kermit="light green";
+var frogName="Kermit";
+console.log(frogName);
+//Double quote or single quote?
+// can use an escaping character \ backslash in front of the apostrophe
+var phrase='I do not know!';
+console.log(phrase);
+
+// Multi - lines by using \n - new line character
+var phrase2 = "I don't know! \nYou never know";
+console.log(phrase2);
+
+
+
+
+
 
 
