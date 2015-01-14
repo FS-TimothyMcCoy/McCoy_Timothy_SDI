85f8870221906ecb186db2373918c4f9e0eb43fc
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-13 23:14:11 -0500

    Named variables
    Named variables

--- McCoy_Timothy_Expressions_Assignment/js/Day 4_homework.js
+++ McCoy_Timothy_Expressions_Assignment/js/Day 4_homework.js
@@ -5,4 +5,10 @@
  1/13/15
  Day 4 Homework
  */
-
+alert("Testing to see if it works")
+///List your variables and values.
+var a = prompt("We are trying to calculate the perimeter of a triangle." +
+"Input your number for value a.");
+var b = prompt("Input your value for side b.");
+var c = prompt("Input your value for side c");
+var perimeterTriangle = Number(a) + Number(b) + Number(c);
