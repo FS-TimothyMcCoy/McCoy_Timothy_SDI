3e40c7c38fd1b6694e7c831e1acce73083a8dacc
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-15 23:03:32 -0500

    Started on group 2
    Started on check the logic group 2

--- Conditionals_Worksheet/js/Conditionals_Worksheet.js
+++ Conditionals_Worksheet/js/Conditionals_Worksheet.js
@@ -9,6 +9,22 @@ Conditionals Worksheet
 
  //Group 1 A chance for Gas!
  //Declare variables
- 
+ var gasEfficiency = 12;
+ var gaugeReading = 50;
+ var tankCapacity = 15;
+ var gaugeConverted = gaugeReading / 100;
+ var halfTank = tankCapacity * gaugeConverted;
+ var gasLeft = gasEfficiency * halfTank;
+ if(gasLeft > halfTank){
+console.log("Yes you can make it without stopping for gas!");
+
+ } else{
+console.log("You only have "+gasLeft+" gallons of gas in your tank, better get gas while you can!");
+
+ }
+
+
+//Group 2:Multiple Results
+ //Check the login
 
 
