a314ef714a8fbd7bd394eeb1dbc3cf0cdab5e6b8
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-17 22:27:11 -0500

    All variables
    All of my variables have been declared and its now time to start
    problem solving.

--- McCoy_Timothy_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ McCoy_Timothy_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -5,3 +5,14 @@ stay at home and play video games,
 or go to the arcade with a friend?
 */
 //Declare your variables
+
+var moviesPrice = 20;
+var gameSnacks = 15;
+var arcadeMoney = 10;
+//You will Spend a dollar of your arcade money on your friend if they come with you
+var arcadeMoneyFriend = arcadeMoney--;
+//If you go to the movies your girlfriend will come with you causing you to spend 10 dollars on her
+var moviesGirlfriend = moviesPrice -= 10;
+
+
+
