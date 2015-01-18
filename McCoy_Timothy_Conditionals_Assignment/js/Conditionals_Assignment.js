b4a148744b28711152927866c719bd52fd5384dd
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-17 22:56:57 -0500

    Working on something
    Working on a syntax error in my coding at the moment.

--- McCoy_Timothy_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ McCoy_Timothy_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -3,9 +3,13 @@
 Will Josh go to the movies with friends,
 stay at home and play video games,
 or go to the arcade with a friend?
+Well it all depends on how his wallet feels about this.
 */
 //Declare your variables
-
+var movies = ("The Movies");
+var location
+var arcade = ("The Arcade");
+var videoGames = ("Video games");
 var moviesPrice = 20;
 var gameSnacks = 15;
 var arcadeMoney = 10;
@@ -13,6 +17,17 @@ var arcadeMoney = 10;
 var arcadeMoneyFriend = arcadeMoney--;
 //If you go to the movies your girlfriend will come with you causing you to spend 10 dollars on her
 var moviesGirlfriend = moviesPrice -= 10;
+//Beginning on my conditionals
+var location = prompt("Where do you want to go?");   
+if ((location === movies && location === arcade) && (location === videoGames))  {
+    console.log("Alright then you are going to " +location); 
+} else if (test) {
+    //code
+}
+var joshsWallet = parseInt(prompt("How much money does Josh have?"));
+if (){
+    //code
+}
 
 
 
