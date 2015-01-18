165cb8c03781baf06cd034081c25ba73614e8856
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-17 23:55:30 -0500

    many changes
    Made a lot of changes but i’m almost done

--- McCoy_Timothy_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ McCoy_Timothy_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -5,29 +5,37 @@ stay at home and play video games,
 or go to the arcade with a friend?
 Well it all depends on how his wallet feels about this.
 */
+var payingAttention = parseInt(prompt("Please choose an event and input how much money you will spend on it. If you are not spending any money on it then input the value 0. Just to check if you are reading the prompt please input the value zero now."));
+var correctAnswer = 0;  
+if (payingAttention === correctAnswer) {
+    console.log("Great job user! You have verified that you are paying attention.");
+} else {
+    console.log("You are not currently paying attention. Please try again.");
+}
+
 //Declare your variables
-var movies = ("The Movies");
-var location
-var arcade = ("The Arcade");
-var videoGames = ("Video games");
-var moviesPrice = 20;
-var gameSnacks = 15;
-var arcadeMoney = 10;
-//You will Spend a dollar of your arcade money on your friend if they come with you
-var arcadeMoneyFriend = arcadeMoney--;
-//If you go to the movies your girlfriend will come with you causing you to spend 10 dollars on her
-var moviesGirlfriend = moviesPrice -= 10;
-//Beginning on my conditionals
-var location = prompt("Where do you want to go?");   
-if ((location === movies && location === arcade) && (location === videoGames))  {
-    console.log("Alright then you are going to " +location); 
-} else if (test) {
-    //code
-}
-var joshsWallet = parseInt(prompt("How much money does Josh have?"));
-if (){
-    //code
-}
+var walletLimit = 100; 
+var userMoviePrice = parseInt(prompt("How much money are you spending on the movie?"));
+var userGameSnackPrice = parseInt(prompt("How much money are you spending on snacks?"));
+var userArcadePrice = parseInt(prompt("How much money are you spending at the arcade?"));
+//Start writing your conditionals
+if (userMoviePrice <= walletLimit) {
+    console.log("Because you chose to spend $"+userMoviePrice+".00 on this movie, you are allowed to go!");
+} else if (userGameSnackPrice <= walletLimit){
+    console.log("Because you chose to spend $"+userGameSnackPrice+".00 on your snacks, you are allowed to go!");
+    } else if (userArcadePrice <= walletLimit){
+        console.log("Because you chose to spend $"+userArcadePrice+".00 on this arcade, you are allowed to go!")
+    } else {
+        console.log("Sorry you do not have the sufficient funds to attend this event.");
+    }
+    
+
+
+
+    
+    
+
+
 
 
 
