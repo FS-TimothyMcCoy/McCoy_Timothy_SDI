a6c3915e5454de163aaf17f8bdb20c46c90da092
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-17 22:20:28 -0500

    Thought made
    I have finally thought of something to do for my assignment and i have
    started coding.

--- McCoy_Timothy_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ McCoy_Timothy_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -1,1 +1,7 @@
-alert("Javascript works!!");
+//alert("Javascript works!!");
+/*
+Will Josh go to the movies with friends,
+stay at home and play video games,
+or go to the arcade with a friend?
+*/
+//Declare your variables
