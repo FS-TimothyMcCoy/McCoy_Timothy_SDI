8b3d85dc7373e9770cf8f2008b3cfb91150ab342
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-20 19:18:27 -0500

    made loop
    Made loops

--- Day 7/js/Day 7_Beer.js
+++ Day 7/js/Day 7_Beer.js
@@ -3,4 +3,20 @@ Timothy McCoy
 Day 7 beer
 1/20/15
 */
-alert("Testing 1,2,3!")
+//alert("Testing 1,2,3!")
+//99 bottles of beer on the wall
+for(var i= 99; i>0; i--){
+    
+    
+
+    console.log(i+ " bottles of beer on the wall. "+i+" bottles of beer. Take one down pass it around. "+(i-1)+" Bottles of beer on the wall.");
+    //Add a check for the last round of beers
+    if (i===1) {
+       console.log(i+ " bottle of beer on the wall. "+i+" bottle of beer. Take one down pass it around. No more bottles of beer on the wall. \n The end"); 
+    }else{
+        console.log(i+ " bottles of beer on the wall. "+i+" bottles of beer. Take one down pass it around. "+(i-1)+" Bottles of beer on the wall.");
+        
+        
+    }
+    
+}
