3a7829a1a349cbe2650c1929226a17db696ec636
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-20 23:12:10 -0500

    Finished do while
    Do while loop is complete

--- Loops_Worksheet/js/Loops_Homework.js
+++ Loops_Worksheet/js/Loops_Homework.js
@@ -4,3 +4,24 @@ Timothy McCoy
 Loops worksheet
 */
 //alert("Testing")
+//While Loops
+//Counting how many years you have until you turn 19 (Counting by 1's)
+var age = prompt("How old are you?"); //Recognize age variable
+while(isNaN(age)|| age === ""){
+    
+    age = prompt("You have entered a character that is not a number, \n please try again.");
+}
+
+while(age<19){
+
+    console.log("I am "+age+" years old.");
+    //Change for age variable
+    age++;
+}
+
+
+
+
+
+
+
