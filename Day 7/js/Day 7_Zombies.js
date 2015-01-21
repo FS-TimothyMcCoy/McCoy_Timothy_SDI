eb2629cd66bdf0e09127fb737a16f5e4e4eee331
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-20 20:01:15 -0500

    Zombies
    Completed Zombies loop

---
+++ Day 7/js/Day 7_Zombies.js
@@ -0,0 +1,37 @@
+/*
+Timothy McCoy
+Day 7 Zombie Attack
+1/20/15
+*/
+//alert("WOOOO DO YOU SEE THE ALERT?!")
+
+//Zombie Attack
+//There is a Zombie at fullsail
+//It can bite 4 people a day and turn them into zombies
+//So now the CDC wants to know how many zombies there will be in 8 days
+
+var numZombies = 1; //How many zombie do we have
+var numBites = 4; //Number of bites per zombie per day
+var day = 8; // Number of days
+var daysTakes = 0;
+for(var i=1; i<= day; i++){
+    //How many new zombies get made and how many go bite people
+    //They get bitten then the next day they can bite
+    var newZombies = numBites * numZombies;
+    //Update the total number of zombies at the end of the day
+    numZombies += newZombies;
+    //Report at the end of the day
+    console.log("There are "+numZombies+" zombies at the end of day #"+ i);
+    
+    
+} while(numZombies<=1000000){
+  //New Zombies
+  var newZombies = numZombies * numBites;
+  //Now how many zombies we have
+  numZombies += newZombies;
+  //Add one to the total number of days it takes
+  daysTakes++;
+  
+    
+}
+console.log("It will take "+daysTakes+" days for a million zombies!");
