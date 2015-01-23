0c710e9776e6fe3e220506644531a43ae3d24948
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-22 22:59:24 -0500

    Finished with all my work
    Done

--- Functions_Worksheet/js/Functions_Worksheet.js
+++ Functions_Worksheet/js/Functions_Worksheet.js
@@ -4,14 +4,41 @@ Timothy McCoy
 Functions Worksheet
 */
 //alert("ok so this is a test")
+//PROBLEM NUMBER 1
 
 //Circumfrence
 //Calculate the circumfrence of a circle
-function circumfrenceCircle(){
-    var radius = parseInt(prompt("What is the radius?"));
+
+var radius = 1;
+
+function calcCircumf(r){
+    var circumfrence = r*2*Math.PI;
     
     
+    return circumfrence;
+}
+
+var returnedCircumfrence = calcCircumf(radius);
+
+calcCircumf(radius);
+console.log("The circumfrence of the circle is "+returnedCircumfrence);
+
+
+//STUNG!!
+
+
+var weight = 1;
+
+function beeStings(weight){
+    var stingsPer = 8.666666667;
+    var stingCalc = weight * stingsPer;
     
-    
+    return stingCalc;
     
 }
+var returnedStingCalc = beeStings(weight);
+
+console.log("It takes "+returnedStingCalc+" bee stings to kill a "+weight+" pound victim.");
+
+
+
