09b4c6aad01e1f8ce6d059e0cd1e5a74a7653c50
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-22 22:12:59 -0500

    variables
    Making variables for homework problem 1

--- Functions_Worksheet/js/Functions_Worksheet.js
+++ Functions_Worksheet/js/Functions_Worksheet.js
@@ -3,4 +3,15 @@ Timothy McCoy
 1/22/15
 Functions Worksheet
 */
-alert("ok so this is a test")
+//alert("ok so this is a test")
+
+//Circumfrence
+//Calculate the circumfrence of a circle
+function circumfrenceCircle(){
+    var radius = parseInt(prompt("What is the radius?"));
+    
+    
+    
+    
+    
+}
