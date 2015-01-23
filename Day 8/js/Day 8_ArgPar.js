901b9e02106dafef57eaf1dec3f550acc5961b8d
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-22 19:39:51 -0500

    ArgPar
    Made basic ArgPar files

---
+++ Day 8/js/Day 8_ArgPar.js
@@ -0,0 +1,6 @@
+/*
+Timothy McCoy
+Day 8 ArgPar
+1/22/15
+*/
+//alert("Testing breh")
--- Day 8/js/Day 8_Scope.js
+++ Day 8/js/Day 8_Scope.js
@@ -4,3 +4,34 @@ Timothy McCoy
 Scope
 */
 //alert("Testinnng")
+
+//Variable Scope
+//Variables inside and outside of a function
+
+//Try not to use the same variable names
+//However, in large files this is going to be impossible
+
+//Variables created inside of functions can only be seen inside of those functions
+
+//Create a variable or width in our MAIN code
+var width = 5; //Scoped outside of the function - main code
+
+//Create a function that calculates the perimeter of a rectangle
+
+function calcPeri() {
+    var width = 10; // scoped to the function calcPeri
+    console.log("Inside of the function the value of width is "+width);
+    
+    var height = 20;
+    var perimeter = width*2 + height*2;
+    console.log("Inside of function the perimeter is "+perimeter);
+    
+}
+console.log("Before call "+width);
+calcPeri();
+
+console.log("After call "+width);
+
+//THIS WILL NOT WORK
+//BECAUSE YOU CAN NOT ACCESS A VARIABLE DECLARED INSIDE OF A FUNCTION FROM YOUR MAIN CODE
+console.log("After call the perimeter is "+perimeter);
