84a69e140ac198c56a781f528d1a0be60b0fb04b
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-22 20:03:44 -0500

    Basic Return
    Made Basic Return files

--- Day 8/js/Day 8_ArgPar.js
+++ Day 8/js/Day 8_ArgPar.js
@@ -4,3 +4,40 @@ Day 8 ArgPar
 1/22/15
 */
 //alert("Testing breh")
+
+//Arguments - goes in the function call
+//Parameters - goes in the function definition
+//Must have the same number of arguments and parameters...... usually
+
+//Create a function to calculate the area of a rectangle
+function calcArea(w,h) { //Parameters go here
+    //var width = 10;
+    //var height = 20;
+    //var area = width * height;
+    var area = w*h;
+    var results = w+h;
+    
+    console.log("The area is "+area);
+    console.log(results);
+}
+
+
+//Function call
+calcArea(10,20);
+calcArea(5,4);
+
+//Calculate dog Years
+//Put in human years, get out dog years
+
+function dogYears(humanAge){ //humanAge Parameter
+    //Dog year = human years*7
+    var dogAge = humanAge*7;
+    console.log("Dog age is "+dogAge);
+    
+    
+}
+//function call with argument
+dogYears(18);
+dogYears(14);
+var userAge = prompt("How old are you?");
+dogYears(Number(userAge));
---
+++ Day 8/js/Day 8_Return.js
@@ -0,0 +1,6 @@
+/*
+Timothy McCoy
+Day 8 Return
+1/22/15
+*/
+//alert("TEsitngsdnfks")
