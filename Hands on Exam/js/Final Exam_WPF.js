8b9f10444829ec8e8219dcd4410f08d2223df534
FS-TimothyMcCoy <tjfatterstacks@live.com>
2015-01-29 20:16:37 -0500

    Hands on
    Hands on part of my exam completed successfully

--- Hands on Exam/js/Final Exam_WPF.js
+++ Hands on Exam/js/Final Exam_WPF.js
@@ -3,4 +3,35 @@ Timothy McCoy
 1/29/15
 Final Exam WPF
 */
-alert("Hands on time")
+//alert("Hands on time")
+//Declare variables and prompts
+
+var originalCost = prompt("What is the cost of the Xbox?");
+while(originalCost === "" || isNaN(originalCost)){
+originalCost = prompt("Please enter a number and do not leave blank. \nWhat is the cost of the Xbox?");    
+    
+}
+var discount = prompt("What is the discount number for this item?")
+while(discount === "" || discount >100){
+discount = prompt("Please enter a number between 0 and 100 and do not leave blank. \nWhat is the discount number?");
+    
+}
+
+
+
+
+//Start on function
+function calcDiscountCost(oC,d){
+    //make variable for equation
+    
+    
+    var moneySaved  = oC * (d/100);
+    totalPrice = oC - moneySaved;
+    return totalPrice ;
+    
+}
+
+
+var returnedTotal = calcDiscountCost(originalCost,discount);
+console.log("The final cost of an item that costs $"+originalCost+" with a discount of "+discount+"% is $"+returnedTotal);
+
